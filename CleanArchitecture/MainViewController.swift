e1de853fe1b5ac4de2210e61003859e8ddd6ab2d
Costraci Mihail <mihaicostraci3@gmail.com>
2017-03-24 12:02:26 +0200

    Added "FirsViewController" and "SecondViewController" and in storyboard.

---
+++ CleanArchitecture/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by winify on 3/24/17.
+//  Copyright © 2017 com.muume.sdk. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
