d64c535cbb73bad63dbf51b21e36cd4929c596b8
Costraci Mihail <mihaicostraci3@gmail.com>
2017-03-24 10:31:07 +0200

    First commit

---
+++ CleanArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by winify on 3/24/17.
+//  Copyright © 2017 com.muume.sdk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
